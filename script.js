ea1f4047f2a9c678d57ee96bb9a9d1615ce44d2f
09092007geise <105931122+09092007geise@users.noreply.github.com>
2024-08-15 10:31:21 -0300

    www

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
       enunciado:
-        "Como é chamada a defesa de linha do campo",
+        "Como é chamada a defesa de linha do campo?",
       alternativas: [
         {
           texto: "Zagueiro!",
@@ -20,54 +20,54 @@ const perguntas = [
     },
     {
       enunciado:
-        "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        "como se chama azul em inglês?",
       alternativas: [
         {
           texto:
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            "blue.",
           afirmacao: "afirmação",
         },
         {
           texto:
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            "red.",
           afirmacao: "afirmação",
         },
       ],
     },
     {
       enunciado:
-        "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        "qual a vaca mais leiteira entre as alternativas abaixo?",
       alternativas: [
         {
           texto:
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            "holandeza.",
           afirmacao: "afirmação",
         },
         {
           texto:
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+            "nelore.",
           afirmacao: "afirmação",
         },
       ],
     },
     {
       enunciado:
-        "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        "quantas havan existem no Brasil?",
       alternativas: [
         {
           texto:
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            "175.",
           afirmacao: "afirmação",
         },
         {
-          texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+          texto: "207.",
           afirmacao: "afirmação",
         },
       ],
     },
     {
       enunciado:
-        "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        "? ",
       alternativas: [
         {
           texto:
